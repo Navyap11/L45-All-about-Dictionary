759834766abafa72d1e4101117385d961807289e
navyap11 <ashwin.p.parmar@gmail.com>
2025-04-30 16:29:08 +0200

    d

---
+++ check the frequency.py
@@ -0,0 +1,16 @@
+og_diction= {"hi":["2"],
+             "my":["4"],
+             "name":["2"],
+             "is":["1"],
+             "Navya":["2"]}
+
+print("The oG dictionary: "+ str(og_diction))
+
+k=2
+
+result=0
+for key in og_diction:
+    if og_diction[key]==k:
+        result +=1
+
+print("The frequency of 2 is: "+str (result))
